def5da86f21f6648648ff34d9be588febdc9a5bb
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-05 09:37:53 +1100

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+var mainElement = document.querySelector('main#main');
+document.querySelector("main#main").remove();
+
+//Create a new h1 element
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+//Set the id to victory
+newHeader.setAttribute('id', 'victory');
+
+//Set the text of the newly created h1
+newHeader.innerHTML = "Nils is the champion";
